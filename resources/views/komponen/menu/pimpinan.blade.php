6fd420b9a5cd308ec05b436c868555ea6eb33349
Henry Jr Zai <hjuniorzai@gmail.com>
2025-09-12 20:33:01 +0700

    add : menambahkan route untuk pimpinan

--- resources/views/komponen/menu/pimpinan.blade.php
+++ resources/views/komponen/menu/pimpinan.blade.php
@@ -4,6 +4,6 @@
 <li class="{{ Route::currentRouteName() == 'dokter.index' ? 'active' : '' }}"><a class="nav-link" href="{{ route('dokter.index') }}"><i class="fas fa-stethoscope"></i> <span>Dokter</span></a></li>
 <li class="{{ Route::currentRouteName() == 'pasien.index' ? 'active' : '' }}"><a class="nav-link" href="{{ route('pasien.index') }}"><i class="fas fa-bed"></i> <span>Pasien</span></a></li>
 <li class="{{ Route::currentRouteName() == 'obat.index' ? 'active' : '' }}"><a class="nav-link" href="{{ route('obat.index') }}"><i class="fas fa-pills"></i> <span>Obat</span></a></li>
-<li class=""><a class="nav-link" href="{{ route('rawat-jalan.index') }}"><i class="fas fa-file-medical"></i> <span>Rawat Jalan</span></a></li>
-<li class=""><a class="nav-link" href="{{ route('riwayat-pemeriksaan.index') }}"><i class="fas fa-notes-medical"></i> <span>Pemeriksaan</span></a></li>
-<li class=""><a class="nav-link" href="#"><i class="fas fa-pills"></i> <span>Resep Obat</span></a></li>
+<li class="{{ Route::currentRouteName() == 'rawat-jalan.index' ? 'active' : '' }}"><a class="nav-link" href="{{ route('rawat-jalan.index') }}"><i class="fas fa-file-medical"></i> <span>Rawat Jalan</span></a></li>
+<li class="{{ Route::currentRouteName() == 'riwayat-pemeriksaan.index' ? 'active' : '' }}"><a class="nav-link" href="{{ route('riwayat-pemeriksaan.index') }}"><i class="fas fa-notes-medical"></i> <span>Pemeriksaan</span></a></li>
+<li class="{{ Route::currentRouteName() == 'resep-obat.index' ? 'active' : '' }}"><a class="nav-link" href="{{ route('resep-obat.index') }}"><i class="fas fa-pills"></i> <span>Resep Obat</span></a></li>
