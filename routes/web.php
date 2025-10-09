91d16be2baa013e3cbf0756fe8811e77028a4ab4
Henry Jr Zai <hjuniorzai@gmail.com>
2025-10-09 22:18:38 +0700

    add : template landing page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('landing.index');
+})->name('home');
+Route::get('/', function () {
+    return view('landing.index');
+})->name('home');
 Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'prosesLogin'])->name('login.proses');
 Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
